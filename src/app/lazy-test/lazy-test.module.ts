c150ba56de9308e88081447b8f5bab1d8a4ddfcb
Roel <mangelschotsroel@hotmail.com>
2018-02-07 22:43:52 +0100

    fix commented out code

--- src/app/lazy-test/lazy-test.module.ts
+++ src/app/lazy-test/lazy-test.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { LazyTestComponent } from './lazy-test.component';
-import { SharedModule } from '../shared/shared.module';
 import { LazyTestRoutingModule } from './lazy-test-routing.module';
 import { LazyTestSharedModule } from './shared/lazy-test-shared.module';
 
 @NgModule({
   imports: [
-    // SharedModule,
     LazyTestRoutingModule,
     LazyTestSharedModule.forRoot()
   ],
